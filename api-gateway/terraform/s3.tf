d5677328e84cac400c1c110f9d9b9b79c8caf2cf
vajid Hussain <vajidhussain77@gmail.com>
2024-03-28 10:46:46 +0000

    IAC terraform use for create s3 bucket in AWS

---
+++ api-gateway/terraform/s3.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.16"
+    }
+  }
+}
+
+provider "aws" {
+  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  profile                 = "default"
+  region                  = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "creation-storage" {
+  bucket        = "hiper-hive-data"
+  force_destroy = true
+  acl    = "public-read" 
+
+  tags = {
+    Name       = "HiperHive"
+    enviroment = "development"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket-owner" {
+  bucket = aws_s3_bucket.creation-storage.id
+  rule {
+    # control_object_ownership = true
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket-access" {
+  bucket = aws_s3_bucket.creation-storage.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucker-acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket-owner,
+    aws_s3_bucket_public_access_block.bucket-access,
+  ]
+
+  bucket = aws_s3_bucket.creation-storage.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "subfolder-one" {
+  bucket = aws_s3_bucket.creation-storage.id
+  key    = "profile images/"
+}
+
+resource "aws_s3_bucket_public_access_block" "creation-storage" {
+  bucket = aws_s3_bucket.creation-storage.id
+
+  block_public_acls   = false
+  block_public_policy = false
+}
